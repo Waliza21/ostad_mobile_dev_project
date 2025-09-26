4166c859acc746462068ecccf73baee6d06b8d81
Waliza21 <atc.virtuallocker@gmail.com>
2025-09-26 23:09:59 +0600

    New Live test

--- lib/assignment/live_test.dart
+++ lib/assignment/live_test.dart
@@ -1,48 +1,119 @@
 import 'package:flutter/material.dart';
 
-List<Map<String, dynamic>> list = [];
+class LiveTest extends StatelessWidget {
+  LiveTest({super.key});
 
-class LiveTest extends StatefulWidget {
-  const LiveTest({super.key});
-
-  @override
-  State<LiveTest> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<LiveTest> {
   final _formKey = GlobalKey();
-  final nameController = TextEditingController();
-  final numberController = TextEditingController();
 
+  final nameController = TextEditingController();
+
+  final numberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.grey, title: Text('Contact List')),
-      body: Column(
-        children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(controller: nameController, initialValue: 'Name'),
-                TextFormField(
-                  controller: numberController,
-                  initialValue: 'Number',
-                  keyboardType: TextInputType.number,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Contact List', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: 'Hasan',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    TextFormField(
+                      controller: numberController,
+                      decoration: InputDecoration(
+                        hintText: '01745-777777',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1.0),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                  ),
+                  child: Text('Add', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              Card(
+                color: const Color.fromARGB(255, 227, 226, 226),
+
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Jawal', style: TextStyle(color: Colors.red)),
+                  subtitle: Text('01877-777777'),
+                  trailing: Icon(Icons.call, color: Colors.blue),
+                ),
+              ),
+              Card(
+                color: const Color.fromARGB(255, 227, 226, 226),
+
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Ferdous', style: TextStyle(color: Colors.red)),
+                  subtitle: Text('01677-777777'),
+                  trailing: Icon(Icons.call, color: Colors.blue),
+                ),
+              ),
+              Card(
+                color: const Color.fromARGB(255, 227, 226, 226),
+
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Hasan', style: TextStyle(color: Colors.red)),
+                  subtitle: Text('01745-777777'),
+                  trailing: Icon(Icons.call, color: Colors.blue),
+                ),
+              ),
+              Card(
+                color: const Color.fromARGB(255, 227, 226, 226),
+
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Hasan', style: TextStyle(color: Colors.red)),
+                  subtitle: Text('01745-777777'),
+                  trailing: Icon(Icons.call, color: Colors.blue),
+                ),
+              ),
+              Card(
+                color: const Color.fromARGB(255, 227, 226, 226),
+                child: ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Hasan', style: TextStyle(color: Colors.red)),
+                  subtitle: Text('01745-777777'),
+                  trailing: Icon(Icons.call, color: Colors.blue),
+                ),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              list.add({'name': nameController, 'number': numberController});
-            },
-            child: Text('Add'),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
-
